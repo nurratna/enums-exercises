5db80aeb6268f95bc623c0ca5d23bd078fd2dc84
nurratna <nurratnasarii@gmail.com>
2017-11-22 07:19:49 +0700

    count exercise

--- exercises/count_pattern_test.rb
+++ exercises/count_pattern_test.rb
@@ -18,50 +18,74 @@ class CountPatternTest < Minitest::Test
     tally = 0
     numbers.each do |number|
       # Your code goes here
+      tally += 1 if number > 17
     end
     assert_equal 2, tally
   end
 
   def test_count_words_that_are_uppercase
-    skip
+    # skip
     words = ["trousers", "SOCKS", "sweater", "Cap", "SHOE", "TIE"]
     tally = 0
     # Your code goes here
+    words.each do |word|
+      tally += 1 if word == word.upcase
+    end
     assert_equal 3, tally
   end
 
   def test_count_words_ending_in_ing
-    skip
+    # skip
     words = ["thought", "brake", "shin", "juice", "trash"]
     # Your code goes here
+    tally = 0
+    words.each do |word|
+      tally += 1 if word.end_with?("ing")
+    end
     assert_equal 0, tally
   end
 
   def test_count_even_numbers
-    skip
+    # skip
     numbers = [9, 2, 1, 3, 18, 39, 71, 4, 6]
     # Your code goes here
+    tally = 0
+    numbers.each do |number|
+      tally += 1 if number.even?
+    end
     assert_equal 4, tally
   end
 
   def test_count_multiples_of_5
-    skip
+    # skip
     numbers = [2, 5, 19, 25, 35, 67]
     # Your code goes here
+    tally = 0
+    numbers.each do |number|
+      tally += 1 if number % 5 == 0
+    end
     assert_equal 3, tally
   end
 
   def test_count_round_prices
-    skip
+    # skip
     prices = [1.0, 3.9, 5.99, 18.5, 20.0]
     # Your code goes here
+    tally = 0
+    prices.each do |price|
+      tally += 1 if price == price.round.to_f
+    end
     assert_equal 2, tally
   end
 
   def test_count_four_letter_words
-    skip
+    # skip
     words = ["bake", "bark", "corn", "apple", "wart", "bird", "umbrella", "fart"]
     # Your code goes here
+    tally = 0
+    words.each do |word|
+      tally += 1 if word.length == 4
+    end
     assert_equal 6, tally
   end
 
--- exercises/count_test.rb
+++ exercises/count_test.rb
@@ -16,49 +16,68 @@ class CountTest < Minitest::Test
     numbers = [9, 18, 12, 17, 1, 3, 99]
     tally = numbers.count do |number|
       # Your code goes here
+      number > 17
     end
     assert_equal 2, tally
   end
 
   def test_count_words_that_are_uppercase
-    skip
+    # skip
     words = ["trousers", "SOCKS", "sweater", "Cap", "SHOE", "TIE"]
     # Your code goes here
+    tally = words.count do |word|
+      word == word.upcase
+    end
     assert_equal 3, tally
   end
 
   def test_count_words_ending_in_ing
-    skip
+    # skip
     words = ["thought", "brake", "shin", "juice", "trash"]
     # Your code goes here
+    tally = words.count do |word|
+      word.end_with?("ing")
+    end
     assert_equal 0, tally
   end
 
   def test_count_even_numbers
-    skip
+    # skip
     numbers = [9, 2, 1, 3, 18, 39, 71, 4, 6]
     # Your code goes here
+    tally = numbers.count do |number|
+      number.even?
+    end
     assert_equal 4, tally
   end
 
   def test_count_multiples_of_5
-    skip
+    # skip
     numbers = [2, 5, 19, 25, 35, 67]
     # Your code goes here
+    tally = numbers.count do |number|
+      number % 5 == 0
+    end
     assert_equal 3, tally
   end
 
   def test_count_round_prices
-    skip
+    # skip
     prices = [1.0, 3.9, 5.99, 18.5, 20.0]
     # Your code goes here
+    tally = prices.count do |price|
+      price == price.round.to_f
+    end
     assert_equal 2, tally
   end
 
   def test_count_four_letter_words
-    skip
+    # skip
     words = ["bake", "bark", "corn", "apple", "wart", "bird", "umbrella", "fart"]
     # Your code goes here
+    tally = words.count do |word|
+      word.length == 4
+    end
     assert_equal 6, tally
   end
 

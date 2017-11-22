aced726c1d72684dcb1efa8809d1f0a5bcec08d9
nurratna <nurratnasarii@gmail.com>
2017-11-22 15:23:51 +0700

    reduce exercise

--- exercises/reduce_test.rb
+++ exercises/reduce_test.rb
@@ -16,47 +16,68 @@ class ReduceTest < Minitest::Test
     numbers = [28, 12, 38, 1, 91]
     result = numbers.reduce(0) do |difference, number|
       # Your code goes here
+      difference - number
     end
     assert_equal -170, result
   end
 
   def test_multiply_list_of_numbers
-    skip
+    # skip
     numbers = [2, 3, 5, 7]
     # initial value is 1
     # Your code goes here
+    value = 1
+    result = numbers.reduce(value) do |product, number|
+      product * number
+    end
     assert_equal 210, result
   end
 
   def test_capitalize_keywords_in_phrase_one_fish_two_fish_red_fish_blue_fish
-    skip
+    # skip
     keywords = ["fish", "blue"]
     # initial value is 'one fish two fish red fish blue fish'
     # Your code goes here
+    value = 'one fish two fish red fish blue fish'
+    result = keywords.reduce(value) do |phrase, keyword|
+      phrase.gsub(keyword, keyword.upcase)
+    end
     assert_equal 'one FISH two FISH red FISH BLUE FISH', result
   end
 
   def test_divide_560_by_a_bunch_of_numbers
-    skip
+    # skip
     numbers = [2, 2, 2, 5, 7]
     # initial value is 560
     # Your code goes here
+    value = 560
+    result = numbers.reduce(value) do |quotient, number|
+      quotient / number
+    end
     assert_equal 2, result
   end
 
   def test_subtract_smallest_values_from_100
-    skip
+    # skip
     elements = [[8, 5, 3], [1, 9, 11], [4, 7, 2], [19, 34, 6]]
     # initial value is 100
     # Your code goes here
+    value = 100
+    result = elements.reduce(value) do |difference, element|
+      difference - element.min
+    end
     assert_equal 88, result
   end
 
   def test_add_all_the_second_values_together
-    skip
+    # skip
     elements = [["a", 1], ["b", 9], ["c", 21]]
     # initial value is 0
     # Your code goes here
+    value = 0
+    result = elements.reduce(value) do |sum, element|
+      sum + element[1]
+    end
     assert_equal 31, result
   end
 

3f341cc59003aa9d560f998b6e74ab49a71e190c
nurratna <nurratnasarii@gmail.com>
2017-12-24 15:28:54 +0700

    none exercise

--- exercises/none_pattern_test.rb
+++ exercises/none_pattern_test.rb
@@ -18,36 +18,52 @@ class NonePatternTest < Minitest::Test
     not_none_negative = true
     numbers.each do |number|
       # Your code goes here
+      not_none_negative = false if number < 0
     end
     refute not_none_negative
   end
 
   def test_none_are_negative
-    skip
+    # skip
     numbers = [9, 3, 1, 8, 3, 3, 5]
     none_negative = true
     # Your code goes here
+    numbers.each do |number|
+      none_negative = false if number < 0
+    end
     assert none_negative
   end
 
   def test_none_shall_pass
-    skip
+    # skip
     critters = ["elf", "hobbit", "dwarf", "wizard", "human"]
     # Your code goes here
+    none_shall_pass = true
+    critters.each do |critter|
+      none_shall_pass = false if critter == 'shall pass'
+    end
     assert none_shall_pass
   end
 
   def test_one_or_more_shall_pass
-    skip
+    # skip
     phrases = ["go", "left", "can cross", "shall pass", "must stay", "tarried"]
     # Your code goes here
+    none_shall_pass = true
+    phrases.each do |phrase|
+      none_shall_pass = false if phrase == 'shall pass'
+    end
     refute none_shall_pass
   end
 
   def test_none_even
-    skip
+    # skip
     numbers = [3, 9, 15, 21, 19]
     # Your code goes here
+    none_even = true
+    numbers.each do |number|
+      none_even = false if number.even?
+    end
     assert none_even
   end
 
